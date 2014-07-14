8d73fd79e5cde82f8603a3741257ac12abfebbcc
Andrew Stewart <andrew@stwrt.ca>
2014-07-14 10:19:07 -0700

    Actually act a bit like a CLI tool now

--- test/specs/batty.spec.js
+++ test/specs/batty.spec.js
@@ -3,8 +3,4 @@
 var Batty = source("batty");
 
 describe("Batty", function() {
-  it("has a quote", function() {
-    var result = Batty();
-    expect(result).to.eql("Can the maker repair what he makes?");
-  });
 });
