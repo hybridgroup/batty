c98af030c426d39d6ea85a3f5873b4e04e171013
Andrew Stewart <andrew@stwrt.ca>
2014-07-14 11:20:31 -0700

    Better, more testable invocation

--- test/specs/batty.spec.js
+++ test/specs/batty.spec.js
@@ -3,4 +3,19 @@
 var Batty = source("batty");
 
 describe("Batty", function() {
+  beforeEach(function() {
+    stub(console, 'log');
+  });
+
+  afterEach(function() {
+    console.log.restore();
+  });
+
+  context("if no argument is passed", function() {
+    it("prints the help", function() {
+      Batty();
+      expect(console.log).to.be.calledWith("No base URL supplied");
+      expect(console.log).to.be.calledWithMatch("Usage example");
+    });
+  });
 });
